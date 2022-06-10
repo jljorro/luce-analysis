080b8f71c022c15ce985ae5932c4e09f3311591d
Jose Luis Jorro Aragoneses <jljorro@MacBook-Pro-de-Jose.local>
2022-06-10 10:31:24 +0200

    Fixed styles in Activities Site

--- src/components/ActivitiesPage.js
+++ src/components/ActivitiesPage.js
@@ -1,35 +1,37 @@
 import React from 'react';
 
-import { Container, Row, Form, Button } from 'react-bootstrap';
+import { Container, Row, Form, Button, Col } from 'react-bootstrap';
 
 import ActivityTable from "./ActivityTable"
 
 export default function ActivitiesPages() {
     return (
-        <Container>
-        <Row>
-          <h1>Actividades</h1>
-        </Row>
+        <Container className="py-2">
 
         <Row>
-          <Form column="true">
-            <h3>Buscar actividades</h3>
-            <Form.Group controlId="formBasicName">
-              <Form.Label>Nombre</Form.Label>
-              <Form.Control type="text" placeholder="Nombre de la actividad"/>
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Facultad</Form.Label>
-              <Form.Control type="text" placeholder="Nombre de la facultad"/>
-            </Form.Group>
-            <br></br>
-            <Button variant="primary" type="submit" >Buscar</Button>
-            <Button variant="secondary" type="button"> Limpiar</Button>
-          </Form>
+          <Col className='mt-4 p-4 pb-2 bg-light rounded-3'>
+              <h1 className='pb-2'>Actividades</h1>
+              <hr class="bg-danger border-2 border-top border-secondary"></hr>
+              <Row className="pb-3">
+              <Form column="true">
+                <Form.Group controlId="formBasicName">
+                  <Form.Label>Nombre</Form.Label>
+                  <Form.Control type="text" placeholder="Nombre de la actividad"/>
+                </Form.Group>
+                <Form.Group className='mb-2'>
+                  <Form.Label>Facultad</Form.Label>
+                  <Form.Control type="text" placeholder="Nombre de la facultad"/>
+                </Form.Group>
+                
+                <Button className="me-2" variant="primary" type="button" >Buscar</Button>
+                <Button variant="secondary" type="button"> Limpiar</Button>
+              </Form>
+              </Row>
+            
+          </Col>
         </Row>
-
-        <br></br>
-        <Row>
+       
+        <Row className="mt-2 p-4">
           <ActivityTable />
         </Row>
         
